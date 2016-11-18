edd2de65e509c79fdceec6cc87af46b3329f29e9
Markus Richter <marku.online@googlemail.com>
2016-11-18 13:43:05 +0100

    changed sampling script

--- src/make_sample.py
+++ src/make_sample.py
@@ -1,19 +1,31 @@
 import random
-
+import numpy.random as rdm
 import sys
 
+#print header of the file
 print("id;natural1;natural2;float1;integer1")
-size = 1000*1000*100
+
+#set row-count
+size = 1000*1000*10
 if len(sys.argv)>1:
     size=int(sys.argv[1])
 
+#set size of randomly generated Chunks
+CHUNK_SIZE=10000
+nat1=list()
+nat2=list()
+int1=list()
+float1=list()
 
 for i in range(0,size):
-    line=str(i) +";"
-    line=line + str(random.randint(0,1000)) + ";"
-    line = line + str(random.randint(0, 10000000)) + ";"
-    line = line + str(random.uniform(-10,30)) + ";"
-    line += str(random.randint(-1000, 1000))
+    if i%CHUNK_SIZE == 0:
+        nat1 = rdm.random_integers(0, 1000, size)
+        nat2 = rdm.random_integers(0,10*1000*1000,CHUNK_SIZE)
+        int1 = rdm.random_integers(-1000, 1000, CHUNK_SIZE)
+        float1=-10 + 40*rdm.random_sample(CHUNK_SIZE)
+    print(i,nat1[i%CHUNK_SIZE], nat2[i%CHUNK_SIZE], float1[i%CHUNK_SIZE], int1[i%CHUNK_SIZE],sep=";")
+#    line=';'.join([str(i), str(nat1[i%CHUNK_SIZE]), str(nat2[i%CHUNK_SIZE]), str(float1[i%CHUNK_SIZE]), str(int1[i%CHUNK_SIZE])])
 
-    print(line)
+
+ #   print(line)
 
