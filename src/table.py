9b0e133b6a15676b1c8d49f5ab1004e9e0651160
cskinx <cs@skink.eu>
2016-11-18 15:48:11 +0100

    unknown changes

--- src/table.py
+++ src/table.py
@@ -1,26 +1,6 @@
 import pandas as pd
 
-##  abstract
-class DataIter:
-	def next(self):
-		pass
-
-	def hasNext(self):
-		pass
-
-	def reset(self):
-		pass
-
-## abstract
-class DataStore:
-	def get_count(self):
-		pass
-
-	def get_iter(self):
-		pass
-
-## abstract
-class DataTable(DataStore):
+class DataTable:
 	def __init__(self,path="",df=pd.DataFrame()):
 		if path is "":
 			self.table=df #type:pd.DataFrame
