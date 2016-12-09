d429d9e5cba6843c837fe48dbbb0deda3262ea29
cskinx <cs@skink.eu>
2016-12-09 12:37:13 +0100

    updated slider events, graph updates, and refactored GUI code

--- src/UI.py
+++ src/UI.py
@@ -12,6 +12,7 @@ import pandas as pd
 from random import random
 from dateutil import rrule
 import matplotlib.pyplot as plt
+from time import time
 
 
 
@@ -26,13 +27,53 @@ class VisAnaGUI(tk.LabelFrame):
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
         self.plot_tooltip=None
-        self.createWidgets()
+        self.create_widgets()
         root.title("Visual Analyser - Gui")
 
+        ## save data source
+        self.ds = ds
 
-    def createWidgets(self):
+        ## dictionary describing which days are contained in base data
+        ## (for timeline)
+        self.dates_contained = self.build_contained_dict(self.ds.get_base_data().df())
+
+        ## save time of last update to prevent too many
+        ## updates when using sliders
+        self.last_action = time()
+        ## delay after last data update and action (in ms)
+        self.UPDATE_DELAY = 500
+        ## was there an action triggering an update?
+        self.unprocessed_action = False
+        ## calls check_for_update() method after delay has passed
+        self.after(self.UPDATE_DELAY, self.check_for_update)
+        ## dummy variable to ignore first update. sliders trigger an 
+        ## event at startup, which would lead to redundant update of data
+        self.ignore_start_trigger = True
+        ## draw data at startup
+        self.trigger_update()
+
+    ## is called regularly, after the given delay
+    def check_for_update(self):
+        ## if there is an unprocessed action older than
+        ## the given delay, update data
+        if self.unprocessed_action and \
+            (time() - self.last_action) > self.UPDATE_DELAY/1000:
+            ## simply block the very first update...
+            ## (there might be prettier solutions)
+            if self.ignore_start_trigger:
+                self.ignore_start_trigger = False
+            else:
+                ## update data
+                self.trigger_update()
+            self.unprocessed_action = False
+        #print("checking for updates...")
+        self.after(self.UPDATE_DELAY, self.check_for_update)
+
+    ## creates all GUI elements
+    def create_widgets(self):
         self.toolbar=tk.LabelFrame(self)
         self.toolbar.grid(column=0, row=0, sticky=(tk.W, tk.N, tk.E), columnspan=5)
+
 
         self.QUIT = tk.Button(self.toolbar, text="QUIT", fg="red",
                               command=root.destroy)
@@ -46,8 +87,8 @@ class VisAnaGUI(tk.LabelFrame):
         self.filter = tk.LabelFrame(self, bg="#0066ff")
         self.filter.grid(column=0, row=1, sticky=(tk.N, tk.E, tk.W),columnspan=5)
         self.f1 = tk.Label(self.filter, text="HIER STEHT Dann EIN FILTER!!!")
-        #self.f1.pack()
-
+        self.filter.columnconfigure(1, weight=0)
+        self.filter.columnconfigure(0, weight=5)
 
         self.timeline = tk.Label(self, text="HIER STEHT NE TIMELINE!!!", bg="#0066ff")
         self.timeline.grid(column=0, row=2, sticky=(tk.N, tk.E, tk.W),columnspan=5)
@@ -57,9 +98,9 @@ class VisAnaGUI(tk.LabelFrame):
 
         self.history = tk.Text(self, width=50)
         self.history.grid(column=2, row=3, sticky=(tk.N, tk.E, tk.S))
-        for i in range(1,101):
-            self.add_to_history('Line %d of 100' % i)
-        self.history["state"]="disabled"
+        #for i in range(1,101):
+        #    self.add_to_history('Line %d of 100' % i)
+        #self.history["state"]="disabled"
         #self.history.insert('end', "Das hier ist mal passiert!!!!\n")
         
         self.historyslider = tk.Scrollbar(self,orient=tk.VERTICAL, command=self.history.yview)
@@ -69,27 +110,61 @@ class VisAnaGUI(tk.LabelFrame):
         #for child in self.winfo_children(): child.grid_configure(padx=5, pady=5)
         self.add_sliders()
 
+    ## dummy method
     def say_hi(self):
         print("hi there, everyone!")
 
+    ## add line to history window
     def add_to_history(self, text):
         self.history.insert('end', "\n" + text )#+ "\n")
+        self.history.see("end")
+
+    ## draw new data according to current positions of date sliders
+    def trigger_update(self):
+        fromVal = self.startSlider.get()
+        endVal = self.endSlider.get()
+        self.ds.select(out_table="time_filter", attr_name="MasterTime",
+            a=self.dates[fromVal], b=self.dates[endVal])
+        df = self.ds.get_data("time_filter").df()
+
+        self.display_data(df)
+        self.draw_timeline(df)
+
+        self.add_to_history("data update "+str(self.dates[fromVal])+" - "+str(self.dates[endVal]))
+
+    #########
+    ## SLIDER METHODS
+    ## we need separate update functions for the sliders, 
+    ## as we could otherwise not distinguish between changing
+    ## the first or second slider
+    #########
 
     ## triggered by changing startslider value
-    def updateStart(self, event):
+    def update_start(self, event):
         fromVal = self.startSlider.get()
         endVal = self.endSlider.get()
         if endVal < fromVal:
-            self.endSlider.set(self.startSlider.get())
-        self.f1label["text"] = "FROM "+str(self.dates[fromVal])
+            self.endSlider.set(fromVal)
+
+        self.handle_slider_update()
 
     ## triggered by changing startslider value
-    def updateEnd(self, event):
+    def update_end(self, event):
         fromVal = self.startSlider.get()
         endVal = self.endSlider.get()
         if endVal < fromVal:
-            self.startSlider.set(self.endSlider.get())
-        self.f2label["text"] = "TO "+str(self.dates[endVal])
+            self.startSlider.set(endVal)
+
+        self.handle_slider_update()
+
+    ## handle data update event, i.e. a slider changed.
+    def handle_slider_update(self):
+        fromVal = self.startSlider.get()
+        endVal = self.endSlider.get()
+        self.startlabel["text"] = "FROM "+str(self.dates[fromVal])
+        self.endlabel["text"] = "TO "+str(self.dates[endVal])
+        self.unprocessed_action = True
+        self.last_action = time()
 
     ## add sliders to GUI
     def add_sliders(self):
@@ -99,16 +174,19 @@ class VisAnaGUI(tk.LabelFrame):
             until=datetime(2014,12,31,23,59,0)):
             self.dates.append(dt.date())
 
-        self.startSlider = Scale(self.filter, from_=0, to=365, orient=HORIZONTAL, command=self.updateStart)
-        self.endSlider = Scale(self.filter, from_=0, to=365, orient=HORIZONTAL, command=self.updateEnd)
-        self.f1 = self.startSlider
-        self.f1.pack(fill=X)
-        self.f1label = tk.Label(self.filter, text="FROM VALUE")
-        self.f1label.pack()
-        self.f2 = self.endSlider
-        self.f2.pack(fill=X)
-        self.f2label = tk.Label(self.filter, text="TO VALUE")
-        self.f2label.pack()
+        ## init sliders
+        self.startSlider = Scale(self.filter, from_=0, to=364, orient=HORIZONTAL, command=self.update_start)
+        self.startSlider.set(0)
+        self.endSlider = Scale(self.filter, from_=0, to=364, orient=HORIZONTAL, command=self.update_end)
+        self.endSlider.set(364)
+
+        ## add sliders and labels to GUI
+        self.startSlider.grid(column=0, row=0, sticky=(tk.W, tk.N, tk.E))
+        self.startlabel = tk.Label(self.filter, text="FROM VALUE")
+        self.startlabel.grid(column=1, row=0, sticky=(tk.W))
+        self.endSlider.grid(column=0, row=1, sticky=(tk.W, tk.S, tk.E))
+        self.endlabel = tk.Label(self.filter, text="TO VALUE")
+        self.endlabel.grid(column=1, row=1, sticky=(tk.W))
 
     def graph_changed(self):
         pass
@@ -154,28 +232,51 @@ class VisAnaGUI(tk.LabelFrame):
         #points = tuple(zip(xdata[ind], ydata[ind]))
         #print('onpick point:', points)
 
-    def draw_timeline(self, date_contained, shown_dates={}, selected_dates=[]):
+    ## build a dictionary with 365 days as keys
+    ## and boolean values for those days that
+    ## contain non-missing values
+    def build_contained_dict(self, df):
+        #print(df)
+        date_contained = dict()
+        for dt in rrule.rrule(rrule.DAILY,
+            dtstart=datetime(2014,1,1,0,0,0),
+            until=datetime(2014,12,31,23,59,0)):
+            date_contained[dt.date()] = False
+        ## only check for values in 'Small' and 'Large'
+        df = df[["MasterTime", "Small", "Large"]]
+        ## iterate through tuples and check for each day
+        ## if there are non-missing values
+        #print(df)
+        for row in df.itertuples():
+            if not pd.isnull(row.Small) or not pd.isnull(row.Large):
+                day = row.MasterTime.date()
+                date_contained[day] = True
+
+        return date_contained
+
+
+    def draw_timeline(self, df, shown_dates={}, selected_dates=[]):
         ## create value for each day in data,
         ## depending on whether it is selected, shown etc.
+        shown_dates = self.build_contained_dict(df)
+
+        ## prepare data for timeline
         days = []
         values = []
-        for day in sorted(date_contained.keys()):
-            if date_contained[day]:
+        for day in sorted(self.dates_contained.keys()):
+            if self.dates_contained[day]:
                 days.append(day)
                 ##if random() < 0.01:
                 if shown_dates[day]:
                     values.append("blue")
-                    ##if day in selected_dates:
-                    if random() < 0.05:
+                    if day in selected_dates:
+                    #if random() < 0.05:
                         values.append("red")
                 else:
                     values.append("lightskyblue")
 
 
-        #print(days)
-        #print(values)
-        #print(len(values))
-
+        ## plot timeline
         fig = Figure(figsize=(6,1), dpi=100)
         ax = fig.add_subplot(111)
 
@@ -193,6 +294,7 @@ class VisAnaGUI(tk.LabelFrame):
         ax.xaxis.set_ticks_position('bottom')
         ax.get_yaxis().set_ticklabels([])
 
+        ## add to GUI
         self.timeline = FigureCanvasTkAgg(fig, self)
         self.timeline.get_tk_widget().grid(column=0, row=2, sticky=(tk.N, tk.E, tk.W),columnspan=5)
 
@@ -200,32 +302,10 @@ class VisAnaGUI(tk.LabelFrame):
 
 
 ## select data from datasource in this time range
-def filter_data(ds, start_time, end_time):
-    ds.select(out_table="time_filter", attr_name="MasterTime",
-        a=start_time, b=end_time)
-    return ds.get_data("time_filter").df()
-
-## build a dictionary with 365 days as keys
-## and boolean values for those days that
-## contain non-missing values
-def build_contained_dict(df):
-    #print(df)
-    date_contained = dict()
-    for dt in rrule.rrule(rrule.DAILY,
-        dtstart=datetime(2014,1,1,0,0,0),
-        until=datetime(2014,12,31,23,59,0)):
-        date_contained[dt.date()] = False
-    ## only check for values in 'Small' and 'Large'
-    df = df[["MasterTime", "Small", "Large"]]
-    ## iterate through tuples and check for each day
-    ## if there are non-missing values
-    #print(df)
-    for row in df.itertuples():
-        if not pd.isnull(row.Small) or not pd.isnull(row.Large):
-            day = row.MasterTime.date()
-            date_contained[day] = True
-
-    return date_contained
+#def filter_data(ds, start_time, end_time):
+#    ds.select(out_table="time_filter", attr_name="MasterTime",
+#        a=start_time, b=end_time)
+#    return ds.get_data("time_filter").df()
 
 
 
@@ -236,19 +316,5 @@ print("read")
 root = tk.Tk()
 
 app = VisAnaGUI(master=root, ds=ds)
-## display base data at startup
-df = ds.get_base_data().df()
-## which days are in the data?
-date_contained = build_contained_dict(df)
-## example: select only dates in december
-df = filter_data(ds, start_time=datetime(2014,12,1,0,0,0), end_time=datetime(2014,12,31,23,59,0))
-## which days are shown in december?
-date_shown = build_contained_dict(df)
-
-## draw data
-app.display_data(df)
-## draw timeline
-app.draw_timeline(date_contained, date_shown)
-
 
 app.mainloop()
